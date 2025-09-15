3165f588767dc3fa9ea43492474297223700ac27
edgaras99 <edgarasj077@gmail.com>
2025-09-15 12:20:02 +0200

    push

---
+++ tema3/oppgave2.php
@@ -0,0 +1,10 @@
+<?php       /* oppgave 2 */
+/*
+/*programmet skal skrive ut tallene fra 1 til 10 og kvadratene av disse
+*/
+for ($tall=1;$tall<=10;$tall++)
+ {
+ $kvadrat=$tall*$tall; /* kvadratet beregnet */
+ print("$tall har kvadratet $kvadrat <br/>"); /* tallet og kvadratet skrevet ut */
+ }
+?>
