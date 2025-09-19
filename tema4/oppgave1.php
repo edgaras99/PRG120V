a59db79ee44a12ea2075faf58d8eff3bb005d24d
edgaras99 <edgarasj077@gmail.com>
2025-09-19 10:51:32 +0200

    push

--- tema4/oppgave1.php
+++ tema4/oppgave1.php
@@ -1,24 +1,30 @@
-<?php
+<?php /* Oppgave 1 */
 /*
-/* programmet mottar html fra skjema 
-/* programmet sjekker om postnr er korrekt fylt ut 
+/* Programmet mottar postnr fra et HTML-skjema
+/* Programmet sjekker om postnr er korrekt fylt ut
 */
-$postnr=$_post ["postnr"];
-if ($postnr) /*postnr er ikke fylt ut */
+$postnr=$_POST ["postnr"];
+$lovligPostnr=true;
+if (!$postnr) /* postnr er ikke fylt ut */
 {
-    print("postnr er ikke fylt ut <br />");
+$lovligPostnr=false;
+print("Postnr er ikke fylt ut <br />");
 }
 else if (strlen($postnr)!=4) /* postnr består ikke av 4 tegn */
- {
-          print("Postnr best&aring;r ikke av 4 tegn <br />");
-    }
-  else if (!ctype_digit($postnr))  /* minst ett av tegnene er ikke et siffer */
-    {
-       print("Postnr best&aring;r ikke bare av siffre  <br />");
-    }
+{
+$lovligPostnr=false;
+print("Postnr best&aring;r ikke av 4 tegn <br />");
+}
 else
-	{
-		print("Postnr er korrekt fylt ut <br />");
-	}
-
+{
+if (!ctype_digit($postnr)) /* minst ett av tegnene er ikke et siffer */
+{
+$lovligPostnr=false;
+print("Postnr best&aring;r ikke bare av siffre <br />");
+}
+}
+if ($lovligPostnr) /* postnr er korrekt fylt ut */
+{
+print("Postnr er korrekt fylt ut <br />");
+}
 ?>
