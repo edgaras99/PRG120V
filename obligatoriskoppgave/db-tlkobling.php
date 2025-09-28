ba67974ecf46082585b8d1a20b0844991de19aa6
edgaras99 <edgarasj077@gmail.com>
2025-09-28 11:54:53 +0200

    push

---
+++ obligatoriskoppgave/db-tlkobling.php
@@ -0,0 +1,9 @@
+<?php
+$host = getenv('DB_HOST');
+$username = getenv('DB_USER');
+$password = getenv('DB_PASSWORD');
+$database = getenv('DB_DATABASE');
+
+ $db=mysqli_connect($host,$username,$password,$database) or die ("ikke kontakt med database-server");
+    /* tilkobling til database-serveren utført */
+ ?>
