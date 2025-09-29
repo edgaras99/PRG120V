f743c62d4328039510c2960c20bc0c60f2c3625f
edgaras99 <edgarasj077@gmail.com>
2025-09-29 12:00:10 +0200

    push

--- obligatoriskoppgave/visalleklasser.php
+++ obligatoriskoppgave/visalleklasser.php
@@ -1,22 +1,25 @@
 <?php /* vis-alle-klasser */
 /*
-/* Programmet skriver ut alle registrerte 
+   Programmet skriver ut alle registrerte klasser
 */
 include("db-tlkobling.php"); /* tilkobling til database-serveren utført og valg av database foretatt */
+
 $sqlSetning="SELECT * FROM klasse ORDER BY klassekode;";
-$sqlResultat=mysqli_query($db,$sqlSetning) or die ("ikke mulig &aring; hente data fra databasen");
-/* SQL-setning sendt til database-serveren */
+$sqlResultat=mysqli_query($db,$sqlSetning) or die("ikke mulig å hente data fra databasen");
+
 $antallRader=mysqli_num_rows($sqlResultat); /* antall rader i resultatet beregnet */
-print ("<h3>registrert klasser </h3>");
-print ("<table border=1>");
-print ("<tr><th align=left>studiumkode</th> <th align=left>studiumnavn</th> </tr>");
-for ($r=1;$r<=$antallRader;$r++)
-{
-$rad=mysqli_fetch_array($sqlResultat); /* ny rad hentet fra spørringsresultatet */
-$klassekode=$rad["klassekode"];
-$klassenavn=$rad["klassenavn"];
-$studiumkode=$rad["studiumkode"];
-print ("<tr> <td> $klassekode </td> <td> $klassenavn </td> </td> $studiumkode</td> </tr>");
+
+print("<h3>Vis alle klasser</h3>");
+print("<table border=1>");
+print("<tr><th align=left>Klassekode</th><th align=left>Klassenavn</th><th align=left>Studiumkode</th></tr>");
+
+for ($r=1; $r<=$antallRader; $r++) {
+    $rad=mysqli_fetch_array($sqlResultat); /* ny rad hentet fra spørringsresultatet */
+    $klassekode=$rad["klassekode"];
+    $klassenavn=$rad["klassenavn"];
+    $studiumkode=$rad["studiumkode"];
+
+    print("<tr><td>$klassekode</td><td>$klassenavn</td><td>$studiumkode</td></tr>");
 }
-print ("</table>");
+print("</table>");
 ?>
