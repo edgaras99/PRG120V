352afa1dea99b0b841b991f161cc65c3a29697e6
edgaras99 <edgarasj077@gmail.com>
2025-10-07 02:43:42 +0200

    p

--- obligatoriskoppgave/dynamiske-funksjoner.php
+++ obligatoriskoppgave/dynamiske-funksjoner.php
@@ -1,17 +1,13 @@
 <?php 
-function listeboksKlassekodestudent ()
-{
-include("db-tlkobling.php"); /* tilkobling til database-server og valg av database utført */
-$sqlSetning="SELECT * FROM klasse ORDER BY klassekode;";
-$sqlResultat=mysqli_query($db,$sqlSetning) or die ("ikke mulig &aring; hente data fra databasen");
-$antallRader=mysqli_num_rows($sqlResultat); /* antall rader i resultatet beregnet */
-for ($r=1;$r<=$antallRader;$r++)
-{
-$rad=mysqli_fetch_array($sqlResultat); /* ny rad hentet fra spørringsresultatet */
-$klassekode=$rad["klassekode"];
-print("<option value='$klassekode'></option>"); /* ny verdi i listeboksen
-laget */
-}
+function listeboksKlassekodestudent() {
+    include("db-tlkobling.php"); 
+    $sqlSetning = "SELECT * FROM klasse ORDER BY klassekode;";
+    $sqlResultat = mysqli_query($db, $sqlSetning) or die("Ikke mulig å hente data fra databasen");
+
+    while ($rad = mysqli_fetch_assoc($sqlResultat)) {
+        $klassekode = $rad["klassekode"];
+        print("<option value='$klassekode'>$klassekode</option>");
+    }
 }
 function listeboksKlassekode ()
 {
